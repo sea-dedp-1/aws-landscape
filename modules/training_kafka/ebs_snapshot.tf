0c7da3819b807ed6fa9ababcb3ffbce548ffbe80
junior-seekasia <junior.c@seekasia.com>
2021-12-16 15:47:54 +0700

    TWDESEA-7 Junior, Jose, Rong Ying: Fix terraform format in ebs_snapshot

--- modules/training_kafka/ebs_snapshot.tf
+++ modules/training_kafka/ebs_snapshot.tf
@@ -1,5 +1,5 @@
 resource "aws_ebs_snapshot" "kafka_ebs_snapshot" {
-  volume_id = aws_instance.kafka.root_block_device.0.volume_id
+  volume_id = "${aws_instance.kafka.root_block_device.0.volume_id}"
 
   tags = {
     Name = "Kafka EBS ${formatdate("YYYY-MM-DD-HH-mm", timestamp())}"
