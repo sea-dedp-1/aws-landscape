3519119950fb811f4ba2cb8f7d3b3ded2bf7189f
junior-seekasia <junior.c@seekasia.com>
2021-12-16 15:32:39 +0700

    TWDESEA-7 Junior, Jose, Rong Ying: Create an EBS snapshot for kafka instance

---
+++ modules/training_kafka/ebs_snapshot.tf
@@ -0,0 +1,7 @@
+resource "aws_ebs_snapshot" "kafka_ebs_snapshot" {
+  volume_id = aws_instance.kafka.root_block_device.0.volume_id
+
+  tags = {
+    Name = "Kafka EBS ${formatdate("YYYY-MM-DD-HH-mm", timestamp())}"
+  }
+}
