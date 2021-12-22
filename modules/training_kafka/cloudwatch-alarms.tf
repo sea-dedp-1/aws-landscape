308e10d77f3a5b4538be09749fbf0c814425c3be
Jun Wei Ng <junwei.ng@thoughtworks.com>
2021-12-22 11:54:30 +0800

    [SEA-DEDP-1-7][Jun Wei|Wi Su] Add cloudwatch alarm for kafka disk utilisation

---
+++ modules/training_kafka/cloudwatch-alarms.tf
@@ -0,0 +1,19 @@
+resource "aws_cloudwatch_metric_alarm" "kafka_disk_utilisation_alarm" {
+  alarm_name          = "kafka-disk-utilisation-alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "DiskSpaceUtilization"
+  namespace           = "System/Linux"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description   = "This metric monitors disk space utilization for Kafka"
+  datapoints_to_alarm = "1"
+  treat_missing_data  = "missing"
+
+  dimensions = {
+    Filesystem = "/dev/xvda1"
+    InstanceId = "i-00e0b93a2865d5c5d"
+    MountPath = "/"
+  }
+}
